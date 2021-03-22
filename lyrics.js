819ead925ed23bf693cc1b4d6b3c8aa3a7619668
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-22 16:16:59 +0530

    in lyrics all type of cases included

--- lyrics.js
+++ lyrics.js
@@ -8,9 +8,19 @@ const $=require('cheerio');
         height: 720,
         deviceScaleFactor: 1,
     });
-    await page.goto('https://www.google.com/search?q=without+me+lyrics');
-    const search=await page.$('.SALvLe.farUxc.mJ2Mod');
-    console.log(search);
+    await page.goto('https://www.google.com/search?q=Jeremy Zucker – Comethru feat. Bea Miller (Lyrics)+lyrics');
+    let search=await page.$('.SALvLe.farUxc.mJ2Mod');
+    console.log(1);
+    if(search===null){
+    console.log(2);
+        const search2=await page.click('#wp-tabs-container g-text-expander');
+        const res=await page.$('#wp-tabs-container')
+        const title=await res.$$eval('.ujudUb span',options => options.map(option => option.textContent))
+        console.log(title.join('\n'));
+        // console.log(search2);
+        return;
+    }
+
     const title=await search.$$eval('.ujudUb span',options => options.map(option => option.textContent))
     console.log(title.join('\n'));
 })()
